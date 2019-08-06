8c57c337d836fcb6fcce7130cc9d07d0d75473c8
unknown <echurch05@yahoo.com>
2019-08-06 12:47:38 -0700

    Fixes a bug in the leaderboard and adds a way to view the house leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,60 +1,74 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "leaderboard",
-  aliases: ["board"],
-  description: "Sends the servers leaderboard",
-  async execute(message, args, bot) {
+	name: "leaderboard",
+	aliases: ["board"],
+	description: "Sends the servers leaderboard",
+	async execute(message, args, bot) {
 
-    if (!args[0]) return message.channel.send("Specify the leaderboard you'd like to view!");
+		if (!args[0]) {
+			const guildInfo = bot.guildInfo.get(message.guild.id);
+			const houses = Object.entries(guildInfo.housePoints).sort((a, b) => b[1] - a[1]);
 
-    const leaderboards = {
-      "points": "stats.housePoints",
-      "xp": "stats.lifetimeXp",
-      "merits": "stats.merits",
-      "purchases": "stats.purchases",
-      "forages": "stats.forages",
-      "potions made": "stats.potionsMade",
-      "potions used": "stats.potionsUsed",
-      "beans": "stats.beansEaten",
-      "trivia": "stats.triviaAnswered",
-      "chests": "stats.chestsOpened",
-      "dementors": "stats.dementorsDefeated",
-      "boggarts": "stats.boggartsDefeated",
-      "duels won": "stats.duelsWon",
-      "duels lost": "stats.duelsLost",
-      "max health": "stats.maxHealth",
+			const leaderboardEmbed = new Discord.RichEmbed()
+				.setAuthor("House Points Leaderboard", message.guild.iconURL)
+				.setColor(message.member.displayHexColor)
+				.setTimestamp();
 
-      "galleons": "balance.galleons",
-      "sickles": "balance.sickles",
-      "knuts": "balance.knuts"
-    };
+			for (const house of houses) {
+				leaderboardEmbed.addField(house[0], `${house[1]} house points`);
+			}
 
-    const leaderboardEntries = Object.entries(leaderboards);
-    const leaderboardDetails = leaderboardEntries.find(entry => entry[0].includes(args.join(" ")));
+			return message.channel.send(leaderboardEmbed);
+		}
 
-    if (!leaderboardDetails) return;
+		const leaderboards = {
+			"house points": "stats.housePoints",
+			"xp": "stats.lifetimeXp",
+			"merits": "stats.merits",
+			"purchases": "stats.purchases",
+			"forages": "stats.forages",
+			"potions made": "stats.potionsMade",
+			"potions used": "stats.potionsUsed",
+			"beans": "stats.beansEaten",
+			"trivia": "stats.triviaAnswered",
+			"chests": "stats.chestsOpened",
+			"dementors": "stats.dementorsDefeated",
+			"boggarts": "stats.boggartsDefeated",
+			"duels won": "stats.duelsWon",
+			"duels lost": "stats.duelsLost",
+			"max health": "stats.maxHealth",
 
-    const leaderboardName = leaderboardDetails[0];
-    const leaderboardKey = leaderboardDetails[1];
+			"galleons": "balance.galleons",
+			"sickles": "balance.sickles",
+			"knuts": "balance.knuts"
+		};
 
-    const users = bot.userInfo.array().filter(u => bot.userInfo.get(`${u.guild}-${u.user}`, leaderboardKey) > 0);
-    const sortedUsers = users.sort((a, b) => bot.userInfo.get(`${b.guild}-${b.user}`, leaderboardKey) - bot.userInfo.get(`${a.guild}-${a.user}`, leaderboardKey));
-    const leaderboard = sortedUsers.splice(0, 10);
+		const leaderboardEntries = Object.entries(leaderboards);
+		const leaderboardDetails = leaderboardEntries.find(entry => entry[0].includes(args.join(" ")));
 
-    const leaderboardEmbed = new Discord.RichEmbed()
-      .setAuthor(`${formatLeaderboardName(leaderboardName)} Leaderboard`, message.guild.iconURL)
-      .setColor(message.member.displayHexColor)
-      .setTimestamp();
+		if (!leaderboardDetails) return;
 
-    for (const user of leaderboard) {
-      leaderboardEmbed.addField(message.guild.members.get(user.user).displayName, `${bot.userInfo.get(`${user.guild}-${user.user}`, leaderboardKey)} ${formatLeaderboardName(leaderboardName)}`);
-    }
+		const leaderboardName = leaderboardDetails[0];
+		const leaderboardKey = leaderboardDetails[1];
 
-    message.channel.send(leaderboardEmbed);
+		const users = bot.userInfo.array().filter(u => bot.userInfo.get(`${u.guild}-${u.user}`, leaderboardKey) > 0 && u.guild === message.guild.id);
+		const sortedUsers = users.sort((a, b) => bot.userInfo.get(`${b.guild}-${b.user}`, leaderboardKey) - bot.userInfo.get(`${a.guild}-${a.user}`, leaderboardKey));
+		const leaderboard = sortedUsers.splice(0, 10);
 
-    function formatLeaderboardName(name) {
-      return name.split(/ +/).map(i => i.charAt(0).toUpperCase() + i.slice(1)).join(" ");
-    }
-  }
+		const leaderboardEmbed = new Discord.RichEmbed()
+			.setAuthor(`${formatLeaderboardName(leaderboardName)} Leaderboard`, message.guild.iconURL)
+			.setColor(message.member.displayHexColor)
+			.setTimestamp();
+
+		for (const user of leaderboard) {
+			leaderboardEmbed.addField(message.guild.members.get(user.user).displayName, `${bot.userInfo.get(`${user.guild}-${user.user}`, leaderboardKey)} ${formatLeaderboardName(leaderboardName)}`);
+		}
+
+		message.channel.send(leaderboardEmbed);
+
+		function formatLeaderboardName(name) {
+			return name.split(/ +/).map(i => i.charAt(0).toUpperCase() + i.slice(1)).join(" ");
+		}
+	}
 };
