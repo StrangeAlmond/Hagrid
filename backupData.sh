1f35be7b57659cee9a1cdfb6eddb81346e5613ec
StrangeAlmond <strangealmond@strangealmond.com>
2020-01-30 09:13:10 -0800

    Updates the data backup script

--- backupData.sh
+++ backupData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TIME=`date +%b-%d-%y`            # This Command will add the date in the backup file's name.
 FILENAME=backup-$TIME.tar.gz    # Backup file name format.
-SRCDIR=/home/strangealmond/Hagrid/data/                         # Location of data to be backed up.
-DESDIR=/home/strangealmond/Hagrid/data_backups/            # Destination of backup file.
+SRCDIR=/home/strangealmond/Discord-Bots/Hagrid/data/                         # Location of data to be backed up.
+DESDIR=/home/strangealmond/Discord-Bots/Hagrid/data_backups/            # Destination of backup file.
 tar -cpzf $DESDIR/$FILENAME $SRCDIR
 
