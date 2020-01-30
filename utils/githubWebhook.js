d5bcff1d234c19328c893c6875927622ea847955
StrangeAlmond <strangealmond@strangealmond.com>
2020-01-30 09:13:56 -0800

    Fixes a bug in the github webhook

--- utils/githubWebhook.js
+++ utils/githubWebhook.js
@@ -2,7 +2,7 @@
 let http = require("http");
 let crypto = require("crypto");
 const exec = require("child_process").exec;
-const logger = require("./logger.js");
+const log = require("./logger.js");
 const botconfig = require("../botconfig.json");
 
 const secret = botconfig.githubWebhookSecret;
@@ -15,7 +15,7 @@ function createServer() {
 
 			if (req.headers["x-hub-signature"] == sig) {
 				exec("cd " + repo + " && git pull && pm2 reload hagrid");
-				logger.log("Pulled a commit from github.", "info");
+				log("Pulled a commit from github.", "info");
 			}
 		});
 
