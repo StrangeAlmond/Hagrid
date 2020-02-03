c16160bb31ba7e665b751f84a3bec624c0d6fe11
StrangeAlmond <strangealmond@strangealmond.com>
2020-02-02 17:35:02 -0800

    Updates the start command to make it more readable

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,8 +1,4 @@
 const Discord = require("discord.js");
-const encountersFile = require("../jsonFiles/forbidden_forest/encounters.json");
-const ambushesFile = require("../jsonFiles/forbidden_forest/ambushes.json");
-const itemsFile = require("../jsonFiles/forbidden_forest/items.json");
-const fs = require("fs");
 
 module.exports = {
 	name: "start",
@@ -51,50 +47,30 @@ module.exports = {
 		}
 
 		usersChannel.createWebhook("Dark Wizard", "https://i.imgur.com/oXahnDf.png");
+		usersChannel.createWebhook("Centaur", "https://i.imgur.com/z4n0Jcf.jpg");
 
 		// Tell them the channel has been created
 		message.reply(`I have created a channel for you to use the maze commands in ${usersChannel}`);
 
-		// Set their encounters and loot bag positions
-		setEncounters();
-		setAmbushes();
-		setLoot();
+		const userData = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
+		const curPos = userData.mazeInfo.curPos;
+		const curMazeLevel = userData.mazeInfo.curMaze;
 
-		// Create the attachment for their current location
+		// Create an attachment for their current location
 		let attachment;
-		if (bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.itemPositions").includes(bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curPos"))) {
-			attachment = new Discord.Attachment(`../images/forbidden_forest/${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curMaze")}/Active/Forest_${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curPos")}.png`, "map.png");
+		if (bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.itemPositions").includes(curPos)) {
+			attachment = new Discord.Attachment(`../images/forbidden_forest/${curMazeLevel}/Active/Forest_${curPos}.png`, "map.png");
 		} else {
-			// Create an attachment for their current location
-			attachment = new Discord.Attachment(`../images/forbidden_forest/${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curMaze")}/Inactive/Forest_${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curPos")}X.png`, "map.png");
+			attachment = new Discord.Attachment(`../images/forbidden_forest/${curMazeLevel}/Inactive/Forest_${curPos}X.png`, "map.png");
 		}
 
 		// Send their current position and guide on how to navigate the maze to the newly created maze channel
-		usersChannel.send(`Use \`${bot.prefix}move up\` (or \`${bot.prefix}u\`) to move up.Use \`${bot.prefix}move down\` (or \`${bot.prefix}d\`) to move down.\nUse \`${bot.prefix}move left\` (or \`${bot.prefix}l\`) to move left.\nUse \`${bot.prefix}move right\` (or \`${bot.prefix}r\`) to move right.\nUse \`${bot.prefix}leave\` to delete this channel when you are done.`, attachment);
-
-		// Set the positions for encounters
-		function setEncounters() {
-			const encounters = encountersFile.filter(e => e.tiles.some(b => fs.readdirSync(`../images/forbidden_forest/${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curMaze")}/Active`).some(f => f.includes(b))));
-			const tiles = encounters.map(e => e.tiles[Math.floor(Math.random() * e.tiles.length)]);
-
-			bot.userInfo.set(`${message.guild.id}-${message.author.id}`, tiles, "mazeInfo.encounterPositions");
-		}
-
-		// Set ambushes such as acromantuals or bandits
-		function setAmbushes() {
-			const ambushes = ambushesFile.filter(e => e.tiles.some(b => fs.readdirSync(`../images/forbidden_forest/${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curMaze")}/Active`).some(f => f.includes(b))));
-			const tiles = ambushes.map(a => a.tiles[Math.floor(Math.random() * a.tiles.length)]);
-
-			bot.userInfo.set(`${message.guild.id}-${message.author.id}`, tiles, "mazeInfo.ambushPositions");
-		}
-
-		// Set the positions for the loot bags
-		function setLoot() {
-			const items = Object.values(itemsFile).filter(a => a.tiles.some(b => fs.readdirSync(`../images/forbidden_forest/${bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.curMaze")}/Active`).some(c => c.includes(b))));
-			const tiles = items.map(i => i.tiles[Math.floor(Math.random() * i.tiles.length)]);
-
-			bot.userInfo.set(`${message.guild.id}-${message.author.id}`, tiles, "mazeInfo.itemPositions");
-		}
+		usersChannel.send(`Use \`${bot.prefix}move up\` (or \`${bot.prefix}u\`) to move up.
+		Use \`${bot.prefix}move down\` (or \`${bot.prefix}d\`) to move down.
+		Use \`${bot.prefix}move left\` (or \`${bot.prefix}l\`) to move left.
+		Use \`${bot.prefix}move right\` (or \`${bot.prefix}r\`) to move right.
+		
+		Use \`${bot.prefix}leave\` to delete this channel when you are done.`, attachment);
 
 		function formattedName() {
 			return message.member.displayName.toLowerCase().replace(/[^a-z0-9+ ]+/gi, "").split(/ +/).join("-");
