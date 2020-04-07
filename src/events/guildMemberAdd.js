16b9685010fe5a95541ee7d53076797a8365cb85
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 15:42:48 -0700

    Updates the guildMemberAdd event

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -13,11 +13,9 @@ module.exports = async (bot, member) => {
   ];
 
   roles.forEach(async role => {
-    const roleObject = await member.guild.roles.find(r => r.name === role);
-    member.roles
-      .add(roleObject)
-      .catch(e =>
-        bot.log(`Error adding roles to new member: ${e.stack}`, "error")
-      );
+    const roleObject = await member.guild.cache.roles.find(r => r.name === role);
+    member.roles.add(roleObject).catch(e =>
+      bot.log(`Error adding roles to new member: ${e.stack}`, "error")
+    );
   });
 };
