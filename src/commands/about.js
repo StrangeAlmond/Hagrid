86f4e3d356793ed9d9747721c48d6045be2398cd
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 12:24:06 -0700

    Updates the about command

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,36 +1,29 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
-const ms = require("parse-ms");
 
 module.exports = {
 	name: "about",
 	description: "About hagrid and the people who worked on it.",
 	async execute(message, args, bot) {
-		// Get the uptime
-		const uptimeObject = ms(Date.now() - bot.uptime);
-		const uptime = `${uptimeObject.hours}h, ${uptimeObject.minutes}m, ${uptimeObject.seconds}s`;
+		const uptime = bot.functions.parseMs(bot.uptime);
+		const uptimeMsg = `${uptime.days}d, ${uptime.hours}h, ${uptime.minutes}m, ${uptime.seconds}s`;
 
-		// Get the tags of the developer and the designer
-		const developer = bot.users.get("356172624684122113").tag;
-		const designer = bot.users.get("137269251361865728").tag;
-
-		// An array of user IDs to get tags from
+		const developer = bot.users.cache.get(bot.ownerId).tag;
+		const designer = bot.users.cache.get("137269251361865728").tag;
 		const specialThanks = ["330204257284521985", "339502515345293314", "362103171771793408", "341269487754149889"];
 
-		// Create a RichEmbed
-		const statsEmbed = new Discord.RichEmbed()
+		const statsEmbed = new Discord.MessageEmbed()
 			.setColor(message.member.displayHexColor)
 			.setThumbnail(bot.user.displayAvatarURL)
-			.addField("Uptime", `${uptime}`)
+			.addField("Uptime", `${uptimeMsg}`)
 			.addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
 			.addField("Version", botconfig.version)
 			.addField("Library", `Discord.js v${Discord.version}`)
 			.addField("Node.js", `${process.version}`)
-			.addField("Credits", `Programmer: **${developer}**\nArtist and Designer: **${designer}**\n\nA special thanks to:\n**${specialThanks.map(u => bot.users.get(u).tag).join("\n")}**`)
+			.addField("Credits", `Programmer: **${developer}**\nArtist and Designer: **${designer}**\n\nA special thanks to:\n**${specialThanks.map(u => bot.users.cache.get(u).tag).join("\n")}**`)
 			.setFooter("© 2019 StrangeAlmond#0001", bot.user.displayAvatarURL)
 			.setTimestamp();
 
-		// Send the RichEmbed
 		message.channel.send(statsEmbed);
 	},
 };
