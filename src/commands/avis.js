1ff1a3c78b10fc23894e9350bcbb06c9b2b3133a
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 14:27:04 -0700

    Updates the avis command

--- src/commands/avis.js
+++ src/commands/avis.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: "avis",
 	description: "Releases a flock of birds from your wand",
 	async execute(message) {
-		// Bird spell easter egg
 		message.channel.send("🐦🐦🐦🐦🐦🐦🐦🐦🐦");
 	},
 };
