360dabfc74a63c2cefeecdbeefd74535f14aefc9
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-08 11:13:58 -0700

    Updates the coin command

--- src/commands/coin.js
+++ src/commands/coin.js
@@ -3,11 +3,8 @@ module.exports = {
 	description: "Flip a coin.",
 	aliases: ["flipcoin", "coinflip"],
 	async execute(message, args, bot) {
-		// Choices
 		const choices = ["Heads", "Tails"];
-		// Pick a choice
-		const choice = choices[Math.floor(Math.random() * (choices.length - 0)) + 0];
-		// Send the result
+		const choice = choices[Math.floor(Math.random() * choices.length)];
 		message.channel.send(`I pick ${choice}!`);
 	},
 };
