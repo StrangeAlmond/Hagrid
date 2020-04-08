10dbfe7b5a1e0ef8faa60eb702c724f2c8d9a66b
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 20:41:21 -0700

    Updates the cards command

--- src/commands/cards.js
+++ src/commands/cards.js
@@ -7,22 +7,35 @@ module.exports = {
 	aliases: ["collectorsCards", "collectorCards"],
 	async execute(message, args, bot) {
 		const user = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
-		if (!user.collectorsItems || !user.collectorsItems.cards) return message.channel.send("You don't have any collectors cards! You can find some by buying chocolate frogs in Hogsmeade and opening them.");
+		if (!user.collectorsItems || !user.collectorsItems.cards) {
+			return message.channel.send("You don't have any collectors cards! You can find some by buying chocolate frogs in Hogsmeade and opening them.");
+		}
 
 		const rareCards = user.collectorsItems.cards.filter(c => c.rarity == "rare");
 		const uncommonCards = user.collectorsItems.cards.filter(c => c.rarity == "uncommon");
 		const commonCards = user.collectorsItems.cards.filter(c => c.rarity == "common");
 
-		const msg = `${rareCards.length > 0 ? `**Rare:**\n${rareCards.map(c => bot.fromCamelCase(c.name)).join("\n")}\n\n${rareCards.length}/${collectorsCards.rare.length} Rare Cards Collected` : ""}
+		const msg = `
+${rareCards.length > 0 ? `**Rare:**
+${rareCards.map(c => bot.functions.fromCamelCase(c.name)).join("\n")}
 
-${uncommonCards.length > 0 ? `**Uncommon:**\n${uncommonCards.map(c => bot.fromCamelCase(c.name)).join("\n")}\n\n${uncommonCards.length}/${collectorsCards.uncommon.length} Uncommon Cards Collected` : ""}
+${rareCards.length}/${collectorsCards.rare.length} Rare Cards Collected` : ""}
 
-${commonCards.length > 0 ? `**Common:**\n${commonCards.map(c => bot.fromCamelCase(c.name)).join("\n")}\n\n${commonCards.length}/${collectorsCards.common.length} Common Cards Collected` : ""}
+${uncommonCards.length > 0 ? `**Uncommon:**
+${uncommonCards.map(c => bot.functions.fromCamelCase(c.name)).join("\n")}
 
-${user.collectorsItems.cards.length}/${collectorsCards.rare.length + collectorsCards.uncommon.length + collectorsCards.common.length} Collected Total.`;
+${uncommonCards.length}/${collectorsCards.uncommon.length} Uncommon Cards Collected` : ""}
+
+${commonCards.length > 0 ? `**Common:**
+${commonCards.map(c => bot.functions.fromCamelCase(c.name)).join("\n")}
+
+${commonCards.length}/${collectorsCards.common.length} Common Cards Collected` : ""}
+
+${user.collectorsItems.cards.length}/${collectorsCards.rare.length + collectorsCards.uncommon.length + collectorsCards.common.length} Collected Total.
+`;
 
 		const embed = new Discord.RichEmbed()
-			.setAuthor("Your Collectors Cards", message.author.displayAvatarURL)
+			.setAuthor("Your Collectors Cards", message.author.displayAvatarURL())
 			.setDescription(msg)
 			.setColor(message.member.displayHexColor)
 			.setFooter(`To view a specific card, use \`${bot.prefix}card <card name>\``)
