54b985814fb26548d3f32244ff09a584ae8e86a7
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 20:48:42 -0700

    Updates the character command

--- src/commands/character.js
+++ src/commands/character.js
@@ -5,10 +5,7 @@ module.exports = {
 	description: "Get information about a random harry potter character.",
 	aliases: ["randomcharacter"],
 	async execute(message) {
-		// Get a random harry potter character and send their info
-		const HpCharacter = nerds.resolve("Harry Potter").asArray()[0];
-
-		// Send info about that character
-		message.channel.send(`Name: ${HpCharacter.full}\nBlood Status: ${HpCharacter.blood_status}\nBirthday: ${HpCharacter.birthday}\nGender: ${HpCharacter.gender}\nHouse: ${HpCharacter.house}\nWand: ${HpCharacter.wand}`);
+		const character = nerds.resolve("Harry Potter").asArray()[0];
+		message.channel.send(`Name: ${character.full}\nBlood Status: ${character.blood_status}\nBirthday: ${character.birthday}\nGender: ${character.gender}\nHouse: ${character.house}\nWand: ${character.wand}`);
 	},
 };
