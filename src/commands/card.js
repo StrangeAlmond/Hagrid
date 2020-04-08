fd04170bdf21250669b9d6477bd76cb63339fca6
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-07 20:37:28 -0700

    Updates the card command

--- src/commands/card.js
+++ src/commands/card.js
@@ -5,13 +5,15 @@ module.exports = {
 	description: "View a specific Collectors Card",
 	async execute(message, args, bot) {
 		const user = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
-		if (!user.collectorsItems || !user.collectorsItems.cards) return message.channel.send("You don't have any collectors cards! You can find some by buying chocolate frogs in Hogsmeade and opening them.");
+		if (!user.collectorsItems || !user.collectorsItems.cards) {
+			return message.channel.send("You don't have any collectors cards! You can find some by buying chocolate frogs in Hogsmeade and opening them.");
+		}
 
-		const card = user.collectorsItems.cards.find(c => bot.fromCamelCase(c.name).toLowerCase().includes(args.join(" ")));
+		const card = user.collectorsItems.cards.find(c => bot.functions.fromCamelCase(c.name).toLowerCase().includes(args.join(" ")));
 		if (!card) return message.channel.send("You either don't have that card or it doesn't exist!");
 
-		const embed = new Discord.RichEmbed()
-			.setTitle(bot.fromCamelCase(card.name))
+		const embed = new Discord.MessageEmbed()
+			.setTitle(bot.functions.fromCamelCase(card.name))
 			.setImage("attachment://image.png")
 			.setColor(message.member.displayHexColor)
 			.setTimestamp();
@@ -19,7 +21,7 @@ module.exports = {
 		message.channel.send({
 			embed,
 			files: [{
-				attachment: `../images/collectibles/${card.rarity}/${bot.capitalizeFirstLetter(card.name)}.png`,
+				attachment: `../images/collectibles/${card.rarity}/${bot.functions.capitalizeFirstLetter(card.name)}.png`,
 				name: "image.png"
 			}]
 		});
