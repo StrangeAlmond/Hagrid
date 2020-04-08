add96219e0e1b71362491dea088e0dc21e0bab9d
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-08 11:17:23 -0700

    Updates the crucio command

--- src/commands/crucio.js
+++ src/commands/crucio.js
@@ -4,35 +4,29 @@ module.exports = {
 	name: "crucio",
 	description: "The torture curse.",
 	async execute(message, args, bot) {
-		// If they're a staff member don't run the command.
-		if (["prefect", "heads of house", "head girl", "head boy", "deputy headmaster", "headmaster"].some(r => message.member.roles.some(role => r.toLowerCase() === role.name.toLowerCase()))) return;
+		const staffRoles = ["prefect", "heads of house", "head girl", "head boy", "deputy headmaster", "headmaster"];
+		if (staffRoles.some(r => message.member.roles.cache.some(role => r.toLowerCase() == role.name.toLowerCase()))) return;
 
-		// Tell them they're a disappointment
 		message.channel.send("Your spelled failed, and its a good thing it did. -20 points.");
 
-		// Find the users house
-		const house = ["slytherin", "gryffindor", "hufflepuff", "ravenclaw"].find(h => message.member.roles.some(r => r.name.toLowerCase() === h.toLowerCase()));
+		const houses = ["slytherin", "gryffindor", "hufflepuff", "ravenclaw"];
+		const house = houses.find(h => message.member.roles.cache.some(r => r.name.toLowerCase() == h.toLowerCase()));
 		if (!house) return;
 
-		// Smack them across the face with -20 points
 		await bot.guildInfo.math(message.guild.id, "-", 20, `housePoints.${house}`);
 		await bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", 20, "stats.housePoints");
 
-		// Find the #house-cup channel
-		const channel = message.guild.channels.find(c => c.name === "house-cup");
+		const channel = message.guild.channels.cache.find(c => c.name == "house-cup");
 		if (!channel) return;
 
-		// Create the embed for removing their points and send it to the house cup channel
-		const memberEmbed = new Discord.RichEmbed()
+		const memberEmbed = new Discord.MessageEmbed()
 			.setAuthor(`Minus 20 points from ${house} and ${message.member.displayName}!`)
-			.addField("Moderator", "Albus Dumbledore", true)
 			.addField("Reason", "Tried to use a forbidden curse", true)
 			.setColor(message.member.displayHexColor)
 			.setFooter(`${message.member.displayName} tried to use a forbidden curse`)
 			.setTimestamp();
 
-		// Send the embed
-		return bot.quickWebhook(channel, memberEmbed, {
+		return bot.functions.quickWebhook(channel, memberEmbed, {
 			username: "House Cup",
 			avatar: "./images/webhook_avatars/houseCup.png",
 			deleteAfterUse: true
