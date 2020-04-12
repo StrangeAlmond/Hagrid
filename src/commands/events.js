0cc2768bcbb4269c771e14800898aedd525bce5f
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-11 23:21:51 -0700

    Updates the events command

--- src/commands/events.js
+++ src/commands/events.js
@@ -1,14 +1,12 @@
 module.exports = {
 	name: "events",
-	description: "View your server's current active events.",
+	description: "Lists your guild's currently active events.",
 	async execute(message, args, bot) {
-		// Make sure only ChesterLampwick and StrangeAlmond can use this command.
+		// Only bot admins can use this command
 		if (!["356172624684122113", "137269251361865728"].includes(message.author.id)) return;
 
-		// Get a list of the current events and seperate them with a comma (,).
 		const events = bot.guildInfo.get(message.guild.id, "events").join(", ");
-
-		// Send a message with the events
+		if (events.length < 1) return message.channel.send("Your guild has no events active.");
 		message.channel.send(`Your guild currently has the following events active: ${events}`);
 	},
 };
