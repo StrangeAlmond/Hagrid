648e6d5f05642c7276d9bf1129135f2d94e2a53c
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-15 10:17:51 -0700

    Updates the help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,8 +7,8 @@ module.exports = {
 	async execute(message, args, bot) {
 		const embeds = [];
 
-		function createCommandsEmbed(name, commands, page) {
-			const embed = new Discord.RichEmbed()
+		function createCommandsEmbed(name, commands) {
+			const embed = new Discord.MessageEmbed()
 				.setAuthor(name)
 				.setDescription(`**Arguments within < > are required.\nArguments within [ ] are optional.**\n\n${commands}`)
 				.setColor(message.member.displayHexColor)
@@ -21,13 +21,16 @@ module.exports = {
 		const mazeCommands = "**!start** - Create a maze channel for you to use the maze commands in.\n**!leave** - Delete your maze channel.\n**!move <left, right, up, or down>** - Move the direction specified in the maze.\n**!forage** - Forage for an item.\n**!explore** - Explore the maze for any items that may be lying around.";
 		const moneyCommands = "**!daily** - Collect your daily sickles.\n**!weekly** - Collect your weekly galleons (requires cistem aperio).\n**!balance** - View your balance.\n**!shop [shop id]** - View a shop's items.\n**!buy <item id>** - Buy an item from the shop.";
 
-		const funCommands = "**!divine** - Ask the crystal ball a question.\n!cards - View your collectors cards.\n!card <card name> View a collectors card more closely.\n!open <item> - Open an item.\n**!pensieve <reminder> <days:hours:minutes>** - Set a reminder.\n**!pensieve list** - List your reminders.\n**!joke** - Ask Peeves to tell you a joke.\n**!trivia** - Spawn a trivia question (these ones do not give house points).\n**!bean** - Eat a Bertie Botts bean.\n**!illegibilus** - Make text unreadable.";
+		const funCommands = "**!divine** - Ask the crystal ball a question.\n**!cards** - View your collectors cards.\n**!card <card name>** - View a collectors card more closely.\n**!open <item>** - Open an item.\n**!pensieve <reminder> <days:hours:minutes>** - Set a reminder.\n**!pensieve list** - List your reminders.\n**!joke** - Ask Peeves to tell you a joke.\n**!trivia** - Spawn a trivia question (these ones do not give house points).\n**!bean** - Eat a Bertie Botts bean.\n**!illegibilus** - Make text unreadable.";
 		const petCommands = "**!pet** - View your pet\n**!pet feed** - Feed your pet.\n**!pet set-name <name>** - Set your pets nickname.";
 		const potionCommands = "**!recipe [potion]** - View a potions recipe.\n**!brew <potion>** - Brew a potion.";
 
 		const botCommandRoles = ["Headmaster", "Deputy Headmaster", "Heads of House", "Auror", "Support Staff"];
 
-		if (message.member.roles.some(r => botCommandRoles.includes(r.name)) || message.author.id === "356172624684122113") commands += "**!ping** - ping the bot\n**!about** - about the bot\n**!uptime** - check the uptime of the bot\n";
+		if (message.member.roles.cache.some(r => botCommandRoles.includes(r.name)) || message.author.id == "356172624684122113") {
+			commands += "**!ping** - ping the bot\n**!about** - about the bot\n**!uptime** - check the uptime of the bot\n";
+		}
+
 		commands += "**!wiki** - View Hagrid's wiki.\n**!merit <@member>** - Merit a member.\n**!badges** - View the available badges.\n**!rule <rule number>** - View the information on a rule.\n**!schedule** - View the training schedule.\n**!duel <@member>** - Send a duel request to a member.\n**!use <item>** - Use an item.\n**!optout** - Opt out of notifications when you can train.\n**!optin** - Opt in to notification when you can train.";
 
 		createCommandsEmbed("General Commands", commands);
@@ -47,40 +50,36 @@ module.exports = {
 
 		embeds.sort((a, b) => b.description.length - a.description.length);
 
-		message.channel.send(embeds[0]).then(async msg => {
-			await msg.react("◀");
-			await msg.react("▶");
+		const msg = await message.channel.send(embeds[0]);
+		await msg.react("◀");
+		await msg.react("▶");
 
-			const filter = (reaction, user) => ["▶", "◀"].includes(reaction.emoji.name) && user.id == message.author.id;
-			const reactionCollector = msg.createReactionCollector(filter, {
-				time: 120000
-			});
-
-			reactionCollector.on("collect", async collected => {
-				if (collected.emoji.name === "▶") {
-					if (page === embeds.length) return msg.reactions.last().remove(message.author);
-
-					page++;
-
-					await msg.edit(embeds[page - 1]);
-					msg.reactions.last().remove(message.author);
-				} else if (collected.emoji.name === "◀") {
-					if (page === 1) return msg.reactions.first().remove(message.author);
-
-					page--;
-
-					// Edit the message with the new page
-					await msg.edit(embeds[page - 1]);
-					msg.reactions.first().remove(message.author);
-				}
-			});
-
-			reactionCollector.on("end", async () => {
-				await message.react("✅");
-				msg.delete();
-			});
-
+		const filter = (reaction, user) => ["▶", "◀"].includes(reaction.emoji.name) && user.id == message.author.id;
+		const reactionCollector = msg.createReactionCollector(filter, {
+			time: 120000
 		});
 
+		reactionCollector.on("collect", async collected => {
+			if (collected.emoji.name == "▶") {
+				if (page == embeds.length) return msg.reactions.cache.last().users.remove(message.author);
+
+				page++;
+
+				await msg.edit(embeds[page - 1]);
+				msg.reactions.cache.last().users.remove(message.author);
+			} else if (collected.emoji.name == "◀") {
+				if (page == 1) return msg.reactions.cache.first().users.remove(message.author);
+
+				page--;
+
+				await msg.edit(embeds[page - 1]);
+				msg.reactions.cache.first().users.remove(message.author);
+			}
+		});
+
+		reactionCollector.on("end", async () => {
+			await message.react("✅");
+			msg.delete();
+		});
 	},
 };
