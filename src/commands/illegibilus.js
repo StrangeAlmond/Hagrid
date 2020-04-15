b6b9aba2c99ef05830218386febcda3742c57eab
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-15 10:21:16 -0700

    Updates the illegibilus command

--- src/commands/illegibilus.js
+++ src/commands/illegibilus.js
@@ -2,13 +2,9 @@ module.exports = {
 	name: "illegibilus",
 	description: "Make text unreadable.",
 	async execute(message, args, bot) {
-		// Ensure they specify something to reverse
 		if (!args[0]) return message.channel.send("❌ | Please specify something to reverse!");
 
-		// Reverse the string
 		const reversedString = args.join(" ").split("").reverse().join("");
-
-		// Send the reversed string
 		message.channel.send(reversedString);
 	},
 };
