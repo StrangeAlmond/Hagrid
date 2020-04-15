866cb836ee84d2a5ff40c6852bb3a7d25e0b8661
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-15 11:05:00 -0700

    Updates the pensieve command

--- src/commands/pensieve.js
+++ src/commands/pensieve.js
@@ -5,27 +5,32 @@ module.exports = {
 	description: "Reminds you of a message of your choosing at the given time.",
 	aliases: ["remindme", "remembrall"],
 	async execute(message, args, bot) {
-		if (!args[0]) return message.channel.send(`Specify what to remind you! Proper Usage: \`${bot.prefix}pensieve <reminder> <days:hours:minutes>\``);
+		if (!args[0]) {
+			return message.channel.send(`Specify what to remind you! Proper Usage: \`${bot.prefix}pensieve <reminder> <days:hours:minutes>\``);
+		}
 
 		args = message.content.split(/ +/);
 		args.shift();
 
 		const reminder = args.slice(0, args.length - 1).join(" ");
 		let time = args[args.length - 1].match(/\d+:\d+:\d+/);
-
-		if (!time) return message.channel.send(`Specify when to remind you! Proper Usage: \`${bot.prefix}pensieve <reminder> <days:hours:minutes>\``);
+		if (!time) {
+			return message.channel.send(`Specify when to remind you! Proper Usage: \`${bot.prefix}pensieve <reminder> <days:hours:minutes>\``);
+		}
 
 		time = time[0].split(/:/).map(t => parseInt(t));
 
-		const timeObject = moment.tz("America/Los_Angeles").add(time[0], "days").add(time[1], "hours").add(time[2], "minutes");
+		const timeObject = moment.tz("America/Los_Angeles")
+			.add(time[0], "days")
+			.add(time[1], "hours")
+			.add(time[2], "minutes");
 
 		const object = {
 			reminder: reminder,
 			time: timeObject.valueOf()
 		};
 
-		bot.userInfo.push(`${message.guild.id}-${message.author.id}`, object, "reminders");
-
+		bot.userInfo.push(message.author.key, object, "reminders");
 		message.channel.send(`Got it! I will remind you in **${time[0]} days, ${time[1]} hours, and ${time[2]} minutes** to **${reminder}**`);
 	},
 };
