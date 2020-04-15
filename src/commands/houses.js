4220e1e806fda785ffb3db18bbf91c5cf28d27ef
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-15 10:20:20 -0700

    Updates the houses command

--- src/commands/houses.js
+++ src/commands/houses.js
@@ -7,20 +7,20 @@ module.exports = {
 
 		const houses = ["Slytherin", "Gryffindor", "Hufflepuff", "Ravenclaw"];
 
-		houses.sort((a, b) => message.guild.roles.find(r => r.name.toLowerCase() === b.toLowerCase()).members.size - message.guild.roles.find(r => r.name.toLowerCase() === a.toLowerCase()).members.size);
+		houses.sort((a, b) => message.guild.roles.cache
+			.find(r => r.name.toLowerCase() == b.toLowerCase())
+			.members.size - message.guild.roles.cache.find(r => r.name.toLowerCase() == a.toLowerCase()).members.size);
 
-		// Create the embed for the houses
-		const housesEmbed = new Discord.RichEmbed()
+		const housesEmbed = new Discord.MessageEmbed()
 			.setAuthor("Houses")
 			.setColor(message.member.displayHexColor)
 			.setTimestamp();
 
 		houses.forEach(house => {
-			housesEmbed.addField(house, message.guild.roles.find(r => r.name.toLowerCase() === house.toLowerCase()).members.size);
+			housesEmbed.addField(house, message.guild.roles.cache.find(r => r.name.toLowerCase() == house.toLowerCase()).members.size);
 		});
 
-		// Send the embed
-		bot.quickWebhook(message.channel, housesEmbed, {
+		bot.functions.quickWebhook(message.channel, housesEmbed, {
 			username: "Sorting Hat",
 			avatar: "../images/webhook_avatars/sortingHat.png",
 			deleteAfterUse: true
