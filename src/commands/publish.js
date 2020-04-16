bc2a3538e9454915def1d74c60f52c8274bd2841
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-15 18:00:46 -0700

    Updates the publish command

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -3,14 +3,13 @@ module.exports = {
 	description: "Publish a newspaper.",
 	async execute(message, args, bot) {
 		const permittedRoles = ["headmaster", "deputy headmaster"];
-		if (!message.member.roles.some(r => permittedRoles.includes(r.name.toLowerCase()))) return;
+		if (!message.member.roles.cache.some(r => permittedRoles.includes(r.name.toLowerCase()))) return;
 
-		const publishChannel = message.guild.channels.find(c => c.name.includes("daily") && c.name.includes("prophet"));
-
+		const publishChannel = message.guild.channels.cache.find(c => c.name.includes("daily") && c.name.includes("prophet"));
 		if (!publishChannel) return;
 
 		if (!args[0]) {
-			return bot.quickWebhook(message.channel, "Specify something to publish!", {
+			return bot.functions.quickWebhook(message.channel, "Specify something to publish!", {
 				username: "Rita Skeeter",
 				avatar: "./images/ritaSkeeter.png"
 			});
@@ -18,12 +17,12 @@ module.exports = {
 
 		const messageContent = message.content.slice(8).trim();
 
-		bot.quickWebhook(message.channel, "Your message has been published.", {
+		bot.functions.quickWebhook(message.channel, "Your message has been published.", {
 			username: "Rita Skeeter",
 			avatar: "./images/webhook_avatars/ritaSkeeter.jpg"
 		});
 
-		bot.quickWebhook(publishChannel, messageContent, {
+		bot.functions.quickWebhook(publishChannel, messageContent, {
 			username: "Rita Skeeter",
 			avatar: "./images/webhook_avatars/ritaSkeeter.jpg"
 		});
