4652dfa9dcc1179c5d67a21951852b9a30368110
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-17 10:50:36 -0700

    Updates the say command

--- src/commands/say.js
+++ src/commands/say.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "say",
 	description: "Send's a copy of the user's message as Hagrid.",
 	async execute(message, args, bot) {
-		if (!["356172624684122113", "137269251361865728"].includes(message.author.id)) return;
+		if (![bot.ownerId, "137269251361865728"].includes(message.author.id)) return;
 
 		args = message.content.split(/ +/);
 		args.shift();
