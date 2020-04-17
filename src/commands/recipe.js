b48df8ce46dff5f87d8e56c0ac838bc95303c3d2
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-17 10:38:50 -0700

    Updates the recipe command

--- src/commands/recipe.js
+++ src/commands/recipe.js
@@ -7,19 +7,22 @@ module.exports = {
 	async execute(message, args, bot) {
 		const webhookOptions = {
 			username: "Severus Snape",
-			avatar: "./images/webhook_avatars/severusSnape.jpg"
+			avatar: "../images/webhook_avatars/severusSnape.jpg"
 		};
 
-		const userData = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
+		const userData = bot.userInfo.get(message.author.key);
 
 		if (!args[0]) {
 			const embeds = [];
 
 			for (const potion of recipes) {
 				const ingredients = potion.ingredients;
-				const formattedIngredients = ingredients.map(i => `${formatString(i)} ${userData.inventory[i.split(/ +/)[1]] >= parseInt(i.split(/ +/)[0]) ? "✅" : "❌"}`).sort().join("\n");
+				const formattedIngredients = ingredients
+					.map(i => `${formatString(i)} ${userData.inventory[i.split(/ +/)[1]] >= parseInt(i.split(/ +/)[0]) ? "✅" : "❌"}`)
+					.sort()
+					.join("\n");
 
-				const embed = new Discord.RichEmbed()
+				const embed = new Discord.MessageEmbed()
 					.setTitle(`${formatString(potion.potion)} Recipe`)
 					.setDescription(`To brew a ${formatString(potion.potion)}, you need:\n${formattedIngredients}\n\n${!userData.studiedSpells.includes(formatString(potion.potion).toLowerCase()) ? "***Note: You have not studied this potion.***" : ""}`)
 					.setColor(message.member.displayHexColor)
@@ -30,31 +33,31 @@ module.exports = {
 
 			const msg = await message.channel.send(embeds[0]);
 
-			if (embeds.length === 1) return;
+			if (embeds.length == 1) return;
 
 			let page = 1;
 
 			await msg.react("◀");
 			await msg.react("▶");
 
-			const filter = (reaction, user) => ["◀", "▶"].includes(reaction.emoji.name) && user.id === message.author.id;
+			const filter = (reaction, user) => ["◀", "▶"].includes(reaction.emoji.name) && user.id == message.author.id;
 			const reactionCollector = msg.createReactionCollector(filter, {
 				time: 120000
 			});
 
 			reactionCollector.on("collect", async collected => {
-				if (collected.emoji.name === "◀") {
-					if (page === 1) return msg.reactions.first().remove(message.author);
+				if (collected.emoji.name == "◀") {
+					if (page == 1) return msg.reactions.cache.first().users.remove(message.author);
 					page--;
 
 					await msg.edit(embeds[page - 1]);
-					msg.reactions.first().remove(message.author);
-				} else if (collected.emoji.name === "▶") {
-					if (page === embeds.length) return msg.reactions.last().remove(message.author);
+					msg.reactions.cache.first().users.remove(message.author);
+				} else if (collected.emoji.name == "▶") {
+					if (page == embeds.length) return msg.reactions.cache.last().users.remove(message.author);
 					page++;
 
 					await msg.edit(embeds[page - 1]);
-					msg.reactions.last().remove(message.author);
+					msg.reactions.cache.last().users.remove(message.author);
 				}
 			});
 
@@ -65,15 +68,18 @@ module.exports = {
 		if (!potion) return errorMessage("Invalid Potion.");
 
 		const ingredients = potion.ingredients;
-		const formattedIngredients = ingredients.map(i => `${formatString(i)} ${userData.inventory[i.split(/ +/)[1]] >= parseInt(i.split(/ +/)[0]) ? "✅" : "❌"}`).sort().join("\n");
+		const formattedIngredients = ingredients
+			.map(i => `${formatString(i)} ${userData.inventory[i.split(/ +/)[1]] >= parseInt(i.split(/ +/)[0]) ? "✅" : "❌"}`)
+			.sort()
+			.join("\n");
 
-		const recipeEmbed = new Discord.RichEmbed()
+		const recipeEmbed = new Discord.MessageEmbed()
 			.setTitle(`${formatString(potion.potion)} Recipe`)
 			.setDescription(`To brew a ${formatString(potion.potion)}, you need:\n${formattedIngredients}\n\n${!userData.studiedSpells.includes(formatString(potion.potion).toLowerCase()) ? "***Note: You have not studied this potion.***" : ""}`)
 			.setColor(message.member.displayHexColor)
 			.setTimestamp();
 
-		bot.quickWebhook(message.channel, recipeEmbed, webhookOptions);
+		bot.functions.quickWebhook(message.channel, recipeEmbed, webhookOptions);
 
 		function formatString(string) {
 			const formatted = string.replace(/([A-Z])/g, " $1").replace(/^./, str => str.toUpperCase());
