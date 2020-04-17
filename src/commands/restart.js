de10f6a33e0e3c1398fcc8ee3ff48924d8963f78
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-17 10:46:24 -0700

    Updates the restart command

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -2,9 +2,12 @@ module.exports = {
 	name: "restart",
 	description: "Restarts Hagrid.",
 	async execute(message, args, bot) {
-		if (message.author.id !== "356172624684122113") return;
+		if (message.author.id != bot.ownerId) return;
 
-		if (bot.guildInfo.get(message.guild.id, "curTrainingSession")) return message.channel.send("There is currently a training session active.").then(msg => msg.delete(5000) && message.delete(5000));
+		if (bot.guildInfo.get(message.guild.id, "curTrainingSession")) {
+			return message.channel.send("There is currently a training session active.")
+				.then(msg => msg.delete({ timeout: 5000 }) && message.delete({ timeout: 5000 }));
+		}
 
 		await message.channel.send("Restarting...");
 		await process.exit();
