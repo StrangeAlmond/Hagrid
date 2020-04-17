6b543eb063132b67849deb1b9176db23e7dbaa44
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-17 10:40:56 -0700

    Updates the reload command

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -4,20 +4,23 @@ module.exports = {
 	name: "reload",
 	description: "Reload a command.",
 	async execute(message, args, bot) {
-		if (message.author.id !== "356172624684122113") return;
-
+		if (message.author.id !== bot.ownerId) return;
 		if (!args[0]) return message.channel.send("Specify a command to reload!");
 
 		const commandName = args[0];
 
 		const command = bot.commands.get(commandName) || bot.commands.find(c => c.aliases && c.aliases.includes(commandName));
-		if (!command) return message.reply("❌ | That command does not exist").then(msg => msg.delete(5000));
+		if (!command) {
+			return message.channel.send("❌ | That command does not exist")
+				.then(msg => msg.delete({ timeout: 5000 }));
+		}
 
-		await delete require.cache[require.resolve(`./${command.name}.js`)];
-		await bot.commands.delete(command.name);
-		const props = await require(`./${command.name}.js`);
+		delete require.cache[require.resolve(`./${command.name}.js`)];
+		bot.commands.delete(command.name);
+		const props = require(`./${command.name}.js`);
 
-		await bot.commands.set(command.name, props);
-		message.reply(`The ${bot.prefix}${command.name} command has been reloaded.`).then(msg => msg.delete(5000) && message.delete(5000));
+		bot.commands.set(command.name, props);
+		message.channel.send(`The ${bot.prefix}${command.name} command has been reloaded.`)
+			.then(msg => msg.delete({ timeout: 5000 }) && message.delete({ timeout: 5000 }));
 	},
 };
