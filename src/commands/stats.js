2ae69d3987597e74a444755125cc5da27554e9b0
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-21 08:49:17 -0700

    Updates the stats command

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "stats",
 	description: "View your statistics.",
 	async execute(message, args, bot) {
-		const user = bot.getUserFromMention(args[0], message.guild) || message.guild.members.get(args[0]) || message.member;
+		const user = bot.functions.getUserFromMention(args[0], message.guild) || message.guild.members.cache.get(args[0]) || message.member;
 		const usersData = bot.userInfo.get(`${message.guild.id}-${user.id}`);
 
 		const stats = {
@@ -27,11 +27,12 @@ module.exports = {
 		};
 
 		let usersStats = Object.entries(stats).map(i => `**${i[0]}:** ${usersData.stats[i[1]]}`);
+		const lifetimeEarnings = Object.keys(usersData.lifetimeEarnings)
+			.map(i => `${usersData.lifetimeEarnings[i]} ${bot.functions.capitalizeFirstLetter(i)}`).join(", ");
+		usersStats.push(`**Lifetime Earnings:** ${lifetimeEarnings}`);
 
-		usersStats.push(`**Lifetime Earnings:** ${Object.keys(usersData.lifetimeEarnings).map(i => `${usersData.lifetimeEarnings[i]} ${i.charAt(0).toUpperCase() + i.slice(1)}`).join(", ")}`);
-
-		const statsEmbed = new Discord.RichEmbed()
-			.setAuthor(`${user.displayName}'s Stats`, user.user.displayAvatarURL)
+		const statsEmbed = new Discord.MessageEmbed()
+			.setAuthor(`${user.displayName}'s Stats`, user.user.displayAvatarURL())
 			.setColor(user.displayHexColor)
 			.setDescription(usersStats.join("\n"))
 			.setTimestamp();
