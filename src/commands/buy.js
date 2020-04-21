0ee77e6b0e395ef9f80eea7969134bbd88a44856
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-21 12:59:06 -0700

    Updates the buy command

--- src/commands/buy.js
+++ src/commands/buy.js
@@ -5,16 +5,15 @@ module.exports = {
 	description: "Purchase an item.",
 	aliases: ["purchase"],
 	async execute(message, args, bot) {
-		// Stores in diagon alley
 		const stores = ["wand", "books", "clothes", "cauldron", "supplies"];
 
-		// If they don't have access to diagon alley don't let them shop at diagon alley
-		if (stores.includes(args[0]) && !message.member.roles.find(r => r.name.toLowerCase() === "unsorted")) return;
-		// If they still need to withdraw galleons from gringotts don't let them shop at diagon alley yet.
-		if (stores.includes(args[0]) && message.member.roles.find(r => r.name.toLowerCase() == "gringotts")) return message.channel.send("❌ | You need to withdraw galleons from gringotts first!");
+		if (stores.includes(args[0]) && !message.member.roles.cache.find(r => r.name.toLowerCase() == "unsorted")) return;
+		if (stores.includes(args[0]) && message.member.roles.cache.find(r => r.name.toLowerCase() == "gringotts")) {
+			return message.channel.send("❌ | You need to withdraw galleons from gringotts first!");
+		}
 
-		if (["wand", "books", "clothes", "cauldron", "supplies"].includes(args[0])) { // The five item to buy in Diagon Alley
-			const roleNames = { // Role/Store names for the corresponding item
+		if (stores.includes(args[0])) {
+			const roleNames = {
 				wand: "ollivanders",
 				books: "flourish and blotts",
 				clothes: "madam malkins",
@@ -22,7 +21,7 @@ module.exports = {
 				supplies: "wiseacres"
 			};
 
-			const messages = { // Unique store messages based on the item being bought
+			const messages = {
 				wand: "Curious indeed how these things happen. The wand chooses the wizard, remember...I think we must expect great things from you, {author}",
 				books: "This should get you started {author}. Copies of A Beginner's Guide to Transfiguration, A History of Magic, Fantastic Beasts and Where to Find Them, Magical Drafts and Potions, Magical Theory, One Thousand Magical Herbs and Fungi, The Dark Forces: A Guide to Self-Protection, and  of course The Standard Book of Spells, Grade One.",
 				clothes: "This should cover you! Three plain black robes, one pointed hat, one pair gloves, and one fashionable winter cloak. Thank you for visiting Madam Malkin's Robes for All Occasions.",
@@ -30,230 +29,194 @@ module.exports = {
 				supplies: "Here you are {author}, 1 set of crystal phials, 1 set brass scales, and a telescope. Please make sure to visit us again for all your wizarding needs!"
 			};
 
-			const webhookOptions = { // Unique webhook options based on the item being bought
+			const webhookOptions = {
 				wand: {
 					username: "Ollivanders",
-					avatar: "./images/webhook_avatars/ollivanders.jpg"
+					avatar: "../images/webhook_avatars/ollivanders.jpg"
 				},
 
 				books: {
 					username: "Flourish and Blotts",
-					avatar: "./images/webhook_avatars/flourishAndBlotts.png"
+					avatar: "../images/webhook_avatars/flourishAndBlotts.png"
 				},
 
 				clothes: {
 					username: "Madam Malkins",
-					avatar: "./images/webhook_avatars/madamMalkins.png"
+					avatar: "../images/webhook_avatars/madamMalkins.png"
 				},
 
 				cauldron: {
 					username: "Potages",
-					avatar: "./images/webhook_avatars/potages.png"
+					avatar: "../images/webhook_avatars/potages.png"
 				},
 
 				supplies: {
 					username: "Wiseacres",
-					avatar: "./images/webhook_avatars/wiseacres.png"
+					avatar: "../images/webhook_avatars/wiseacres.png"
 				}
 			};
 
-			const shopName = roleNames[args[0]]; // Name of the shop
-			const webhookMessage = messages[args[0]].replace("{author}", `${message.author}`); // Get the unique message for the item being bought and replace any instance of '${author}' with the user's user object
-			const webhookOption = webhookOptions[args[0]]; // Get the unique webhook options for the store that the item's being bought from
+			if (!message.member.roles.cache.some(r => r.name.toLowerCase() == roleNames[args[0]])) return;
 
-			const shopRole = message.guild.roles.find(r => r.name.toLowerCase() === shopName); // Get the role for the shop
+			const shopName = roleNames[args[0]];
+			const webhookMessage = messages[args[0]].replace("{author}", `${message.author}`);
+			const webhookOption = webhookOptions[args[0]];
 
-			if (args[0] === "wand") { // If they're buying a wand then we need to give them the 'wand' role.
-				const wandRole = await message.guild.roles.find(r => r.name.toLowerCase() === "wand"); // Get the wand role
-				message.member.addRole(wandRole); // Give it to the user
+			const shopRole = message.guild.roles.cache.find(r => r.name.toLowerCase() == shopName);
+
+			if (args[0] == "wand") {
+				const wandRole = await message.guild.roles.cache.find(r => r.name.toLowerCase() == "wand");
+				message.member.roles.add(wandRole);
 			}
 
-			// Send the webhook message
-			await bot.quickWebhook(message.channel, webhookMessage, webhookOption);
+			bot.functions.quickWebhook(message.channel, webhookMessage, webhookOption);
+			message.member.roles.remove(shopRole).catch(e => console.error(e));
+		} else if (args[0] == "900") {
+			const userData = bot.userInfo.get(message.author.key);
 
-			// Wait 1.5 seconds before removing their role to give time to read the message
-			setTimeout(() => {
-				message.member.removeRole(shopRole);
-			}, 1500);
-		} else if (args[0] === "900") {
-			// If they're buying floo powder
-			const userData = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
+			const role = message.guild.roles.cache.find(r => r.name.toLowerCase() == "apparition");
+			if (!role) return;
 
-			// Get the apparition role
-			const role = message.guild.roles.find(r => r.name.toLowerCase() === "apparition");
-			if (!role) return; // If the apparition role can't be found then don't execute any further code
-
-			// Get the user's sickles and galleons
 			const sickles = userData.balance.sickles;
 			const galleons = userData.balance.galleons;
 
-			// if they have (< 2) sickles but (> 0) galleons then convert a galleon to 17 sickles
 			if (sickles < 2 && galleons > 0) {
-				bot.userInfo.dec(`${message.guild.id}-${message.author.id}`, "balance.galleons");
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "+", 17, "balance.sickles");
-			} else if (sickles < 2 && galleons <= 0) { // Otherwise if they're just plain broke don't give them the apparition role
+				bot.userInfo.dec(message.author.key, "balance.galleons");
+				bot.userInfo.math(message.author.key, "+", 17, "balance.sickles");
+			} else if (sickles < 2 && galleons <= 0) {
 				return message.channel.send("You can't afford floo powder!");
 			}
 
-			// Remove 2 sickles from the user's balance
-			bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", 2, "balance.sickles");
+			bot.userInfo.math(message.author.key, "-", 2, "balance.sickles");
+			message.member.roles.add(role);
 
-			// Give the user the apparition role
-			message.member.addRole(role);
-
-			// Create an object with the current timestamp and the effect type
 			const object = {
 				time: Date.now(),
 				type: "floo powder"
 			};
 
-			// Push it into the user's active effects
-			bot.userInfo.push(`${message.guild.id}-${message.author.id}`, object, "stats.activeEffects");
-
-			// Let them know everything worked
-			message.channel.send("You have bought floo powder!");
-		} else if ("apparition lessons".includes(args.join(" "))) { // If they're purchasing apparition lessons
-			// Get the user's object
-			const user = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
-			// Make sure they're year 6 or above
+			bot.userInfo.push(message.author.key, object, "stats.activeEffects");
+			message.channel.send("You have purchased floo powder.");
+		} else if ("apparition lessons".includes(args.join(" "))) {
+			const user = bot.userInfo.get(message.author.key);
 			if (user.year < 6) return;
 
-			// Grab the apparition role
-			const apparitionRole = message.guild.roles.find(r => r.name.toLowerCase() === "apparition");
-			if (!apparitionRole) return; // If the apparition role can't be found don't execute any further code.
+			const apparitionRole = message.guild.roles.cache.find(r => r.name.toLowerCase() == "apparition");
+			if (!apparitionRole) return;
 
-			// Make sure they have at least 12 galleons
-			if (user.balance.galleons < 12) return message.channel.send("You need 12 galleons to buy apparition lessons!");
+			if (user.balance.galleons < 12) {
+				return message.channel.send("You need 12 galleons to buy apparition lessons!");
+			}
 
-			// Take away 12 galleons from the user's balance
-			bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", 12, "balance.galleons");
-			// Give the user the apparition role
-			message.member.addRole(apparitionRole);
-			// Let them know everything worked
+			bot.userInfo.math(message.author.key, "-", 12, "balance.galleons");
+			message.member.roles.add(apparitionRole);
 			message.channel.send("You have studied apparition lessons for 12 galleons!");
-		} else { // Otherwise if they're buying a shop item
-			// Make sure it's an actual item
+		} else {
 			if (!items[args[0]]) return message.channel.send("Invalid Item!");
 
-			// Get the user object
-			const user = bot.userInfo.get(`${message.guild.id}-${message.author.id}`);
+			const user = bot.userInfo.get(message.author.key);
 
-			// If they're purchasing a new cauldron
 			if (items[args[0]].name.toLowerCase().includes("cauldron")) {
-				// Make sure they have enough money
-				if (user.balance.galleons < items[args[0]].price) return message.channel.send("You can't afford this item!");
-				// Make sure they have the required cauldron tier
-				if (user.cauldron !== items[args[0]].requires) return message.channel.send(`This cauldron requires you to have the ${items[args[0]].requires} cauldron before buying it`);
+				if (user.balance.galleons < items[args[0]].price) {
+					return message.channel.send("You can't afford this item!");
+				}
 
-				// Get the price of the cauldron
+				if (user.cauldron != items[args[0]].requires) {
+					return message.channel.send(`This cauldron requires you to have the ${items[args[0]].requires} cauldron before buying it`);
+				}
+
 				const price = parseInt(items[args[0]].price.split(/ +/)[0]);
 
-				// Remove x amount of galleons from their balance
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", price, "balance.galleons");
-				// Give them their new cauldron
-				bot.userInfo.set(`${message.guild.id}-${message.author.id}`, items[args[0]].name.toLowerCase().split(/ +/)[0], "cauldron");
-				// Add 1 to their purchases stat
-				bot.userInfo.inc(`${message.guild.id}-${message.author.id}`, "stats.purchases");
+				bot.userInfo.math(message.author.key, "-", price, "balance.galleons");
+				bot.userInfo.set(message.author.key, items[args[0]].name.toLowerCase().split(/ +/)[0], "cauldron");
+				bot.userInfo.inc(message.author.key, "stats.purchases");
 
-				// Let them know it worked
 				message.channel.send(`You have purchased a ${items[args[0]].name}`);
-				// Log that they purchased a new cauldron
 				bot.log(`${message.member.displayName} purchased a ${items[args[0]].name}`, "info");
-			} else if (items[args[0]].type === "item") {
-				// Default amount value
+			} else if (items[args[0]].type == "item") {
 				let amount = 1;
 
-				// If they specify a different amount then set the new amount value
 				if (args[1] && !isNaN(args[1])) {
 					amount = parseInt(args[1]);
 				}
 
-				// Set the price to 0 for now
 				let price = 0;
 
-				// Get the price array
 				const priceArray = items[args[0]].price.split(/, +/);
-				// For each value in the price array
 				priceArray.forEach(p => {
-					p = p.split(/ +/); // Seperate each word into an array
-					if (p[1].startsWith("galleon")) { // If galleons are being used
-						// Add 493 knuts to the price
+					p = p.split(/ +/);
+					if (p[1].startsWith("galleon")) {
 						price += (parseInt(p[0]) * 493);
-					} else if (p[1].startsWith("sickle")) { // If sickles are being used
-						// Add 29 knuts to the price
+					} else if (p[1].startsWith("sickle")) {
 						price += (parseInt(p[0]) * 29);
-					} else if (p[1].startsWith("knut")) { // If knuts are being used
-						// Add x amount of knuts to the price
+					} else if (p[1].startsWith("knut")) {
 						price += parseInt(p[0]);
 					}
 				});
 
-				// Multiply the price to match the amount
-				price = price * amount;
+				price *= amount;
 
-				// Make sure they can afford the item
-				if ((user.balance.sickles * 29) + (user.balance.galleons * 493) < price) return message.channel.send("You can't afford this item.");
-
-				// Convert their money to sickles until they can afford the price
-				for (let i = 0; price > bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "balance.knuts"); i++) {
-					if (bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "balance.sickles") <= 0 && bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "balance.galleons") > 0) {
-						bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "+", 17, "balance.sickles");
-						bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", 1, "balance.galleons");
-					}
-
-					bot.userInfo.dec(`${message.guild.id}-${message.author.id}`, "balance.sickles");
-					bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "+", 29, "balance.knuts");
+				if ((user.balance.sickles * 29) + (user.balance.galleons * 493) < price) {
+					return message.channel.send("You can't afford this item.");
 				}
 
-				// Ensure they have the key in their inventory
-				if (!bot.userInfo.hasProp(`${message.guild.id}-${message.author.id}`, `inventory.${items[args[0]].key}`)) bot.userInfo.set(`${message.guild.id}-${message.author.id}`, 0, `inventory.${items[args[0]].key}`);
+				for (let i = 0; price > bot.userInfo.get(message.author.key, "balance.knuts"); i++) {
+					if (bot.userInfo.get(message.author.key, "balance.sickles") <= 0 && bot.userInfo.get(message.author.key, "balance.galleons") > 0) {
+						bot.userInfo.math(message.author.key, "+", 17, "balance.sickles");
+						bot.userInfo.math(message.author.key, "-", 1, "balance.galleons");
+					}
 
-				// Take away the price amount from their balance
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", price, "balance.knuts");
-				// Increase their purchases stat by the amount value
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "+", amount, "stats.purchases");
+					bot.userInfo.dec(message.author.key, "balance.sickles");
+					bot.userInfo.math(message.author.key, "+", 29, "balance.knuts");
+				}
 
-				// Send a message saying they purchased it
+				if (!bot.userInfo.hasProp(message.author.key, `inventory.${items[args[0]].key}`)) {
+					bot.userInfo.set(message.author.key, 0, `inventory.${items[args[0]].key}`);
+				}
+
+				bot.userInfo.math(message.author.key, "-", price, "balance.knuts");
+				bot.userInfo.math(message.author.key, "+", amount, "stats.purchases");
+
 				message.channel.send(`You have purchased ${amount} ${items[args[0]].name}(s)`);
 
-				if (items[args[0]].id === "1101") amount *= 6;
-				if (items[args[0]].id === "705") amount *= 2;
-				if (items[args[0]].id === "700") amount *= 10;
-				if (items[args[0]].id === "714") amount *= 10;
-				if (items[args[0]].id === "709") amount *= 10;
-				if (items[args[0]].id === "712") amount *= 5;
-				if (items[args[0]].id === "1000") amount *= 5;
+				if (items[args[0]].id == "1101") amount *= 6;
+				if (items[args[0]].id == "705") amount *= 2;
+				if (items[args[0]].id == "700") amount *= 10;
+				if (items[args[0]].id == "714") amount *= 10;
+				if (items[args[0]].id == "709") amount *= 10;
+				if (items[args[0]].id == "712") amount *= 5;
+				if (items[args[0]].id == "1000") amount *= 5;
 
-				// Give them the item they purchased
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "+", amount, `inventory.${items[args[0]].key}`);
-				// Log to the console what they bought and how much
+				bot.userInfo.math(message.author.key, "+", amount, `inventory.${items[args[0]].key}`);
 				bot.log(`${message.member.displayName} purchased ${amount} ${items[args[0]].name}(s)`, "info");
-			} else if (items[args[0]].type === "pet") { // If they're buying a pet
-				// Make sure they don't already have a pet
+			} else if (items[args[0]].type == "pet") {
 				const pets = user.pets.filter(p => !p.retired);
 				const pet = pets[0];
 
-				if (pet && (pet.level != 7 || pet.tier != 1)) return message.channel.send("You already have a pet!");
+				if (pet && (pet.level != 7 || pet.tier != 1)) {
+					return message.channel.send("You already have a pet!");
+				}
 
-				// Make sure they're using the proper id id for a pet
 				if (!items[args[0]]) return message.channel.send("❌ | That's not a pet!");
 
-				// Get the pet object
 				const petObject = items[args[0]];
 
-				if (!pet && petObject.tier == 2) return message.channel.send("You must first display you can take care of a pet by purchasing and taking care of a tier 1 pet!");
+				if (!pet && petObject.tier == 2) {
+					return message.channel.send("You must first display you can take care of a pet by purchasing and taking care of a tier 1 pet!");
+				}
 
-				// Get the pet's price.
 				const price = parseInt(petObject.price.split(/ +/)[0]);
 
-				if (pet && pet.level == 7 && petObject.tier == 1) return message.channel.send("You can only purchase a tier 2 pet!");
-				// Make sure they can afford it
-				if (bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "balance.galleons") < price) return message.channel.send("You can't afford this pet!");
+				if (pet && pet.level == 7 && petObject.tier == 1) {
+					return message.channel.send("You can only purchase a tier 2 pet!");
+				}
 
-				// Available genders to choose from (Note: any other genders requested shall be changed manually.)
+				if (bot.userInfo.get(message.author.key, "balance.galleons") < price) {
+					return message.channel.send("You can't afford this pet!");
+				}
+
 				const genders = ["Male", "Female"];
 
-				// Create an object for the pet
 				const object = {
 					id: Date.now(),
 					lastFeed: null,
@@ -270,13 +233,11 @@ module.exports = {
 				if (pet) pet.retired = true;
 				user.pets.push(object);
 
-				bot.userInfo.set(`${message.guild.id}-${message.author.id}`, user.pets, "pets");
-				bot.userInfo.math(`${message.guild.id}-${message.author.id}`, "-", price, "balance.galleons");
-				bot.userInfo.inc(`${message.guild.id}-${message.author.id}`, "stats.purchases");
+				bot.userInfo.set(message.author.key, user.pets, "pets");
+				bot.userInfo.math(message.author.key, "-", price, "balance.galleons");
+				bot.userInfo.inc(message.author.key, "stats.purchases");
 
-				// Send a message saying it worked
 				message.channel.send(`Congratulations ${message.member.displayName}! You have purchased a ${petObject.name}! Make sure you use !pet feed every day to make sure your pet doesn't faint!`);
-				// Log that they bought a pet to the console
 				bot.log(`${message.member.displayName} purchased a ${items[args[0]].name}`, "info");
 			}
 		}
