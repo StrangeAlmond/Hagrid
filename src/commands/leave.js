2ebea725fce8c21de86bd45e52f9dd362641bc8d
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-20 23:27:48 -0700

    Updates the leave command

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,13 +1,12 @@
+// TODO: test command
 module.exports = {
 	name: "leave",
 	description: "Deletes your maze channel.",
 	aliases: ["exit", "done", "delete"],
 	async execute(message, args, bot) {
-		// Make sure they initiated this channel
-		if (!message.channel.name.includes(message.member.displayName.toLowerCase().replace(/[^a-z0-9+ ]+/gi, "").split(/ +/).join("-")) && message.author.id !== "137269251361865728") return;
-		if (bot.userInfo.get(`${message.guild.id}-${message.author.id}`, "mazeInfo.inFight")) return;
+		if (!bot.functions.isMazeChannel(message.channel.name, message.member)) return;
+		if (bot.userInfo.get(message.author.key, "mazeInfo.inFight")) return;
 
-		// Delete the channel
 		message.channel.delete();
 	},
 };
