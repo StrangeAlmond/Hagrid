fc54e22e1b13e58eb3bef2ba354e2da5c9cf8cbe
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-20 23:11:53 -0700

    Updates the spawn command

--- src/commands/spawn.js
+++ src/commands/spawn.js
@@ -5,28 +5,27 @@ module.exports = {
 	name: "spawn",
 	description: "Spawn a dementor, boggart, chest, or training session.",
 	async execute(message, args, bot) {
-		if (!["356172624684122113", "137269251361865728"].includes(message.author.id)) return;
+		if (![bot.ownerId, "137269251361865728"].includes(message.author.id)) return;
 		if (!args[0]) return message.channel.send("Specify what to spawn!");
 
 		message.delete();
 
-		if (args[0] === "dementor") {
-			bot.spawnDementor(message.channel);
+		if (args[0] == "dementor") {
+			bot.functions.spawnDementor(message.channel, bot);
 
-		} else if (args[0] === "boggart") {
-			bot.spawnBoggart(message.channel);
+		} else if (args[0] == "boggart") {
+			bot.functions.spawnBoggart(message.channel, bot);
 
-		} else if (args[0] === "chest") {
-			bot.spawnChest(message.channel);
+		} else if (args[0] == "chest") {
+			bot.functions.spawnChest(message.channel, bot);
 
-		} else if (args[0] === "training") {
-			const trainingChannel = message.guild.channels.find(c => c.name === "training-grounds");
+		} else if (args[0] == "training") {
+			const trainingChannel = message.guild.channels.cache.find(c => c.name == "training-grounds");
 			if (!trainingChannel) return;
 
 			const trainingSession = new TrainingSession(bot, trainingChannel);
 
-			if (beasts.find(b => b.spell.slice(1) === args[args.length - 1] || b.name.toLowerCase() === args[args.length - 1])) {
-				console.log(args[args.length - 1]);
+			if (beasts.find(b => b.spell.slice(1) == args[args.length - 1] || b.name.toLowerCase() == args[args.length - 1])) {
 				trainingSession.spawnTrainingSession(args[args.length - 1]);
 				return;
 			}
