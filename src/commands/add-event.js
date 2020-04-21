84b99c382042dec25a635de69903a6165897b375
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-21 12:06:08 -0700

    Updates the add-event command

--- src/commands/add-event.js
+++ src/commands/add-event.js
@@ -3,23 +3,21 @@ module.exports = {
 	description: "Add an event to your server's list of active events.",
 	aliases: ["event-add"],
 	async execute(message, args, bot) {
-		// Make sure only ChesterLampwick and StrangeAlmond can use this command.
-		if (!["356172624684122113", "137269251361865728"].includes(message.author.id)) return;
+		if (![bot.ownerId, "137269251361865728"].includes(message.author.id)) return;
 
-		// Possible events that can be added.
 		const events = ["double-xp"];
 
-		// Make sure they specify an event and specify a valid event.
-		if (!args[0]) return message.channel.send(`Specify an event to add! Possible events are: ${events.join(", ")}`);
-		if (!events.includes(args[0])) return message.channel.send(`Invalid event! Possible events: ${events.join(", ")}`);
+		if (!args[0]) {
+			return message.channel.send(`Specify an event to add! Possible events are: ${events.join(", ")}`);
+		}
 
-		// Identify the event they chose.
+		if (!events.includes(args[0])) {
+			return message.channel.send(`Invalid event! Possible events: ${events.join(", ")}`);
+		}
+
 		const event = events.find(e => e === args[0]);
 
-		// Add the event to their guild data.
 		bot.guildInfo.push(message.guild.id, event, "events");
-
-		// Send a message saying that we've added the event
 		message.channel.send(`Got it! I have added the ${event} event!`);
 	},
 };
