07f5264008d8de4c1eefdccac0c0fc0969cfc424
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-20 23:15:09 -0700

    Updates the spell command

--- src/commands/spells.js
+++ src/commands/spells.js
@@ -6,16 +6,21 @@ module.exports = {
 	description: "View your studied spells.",
 	aliases: ["learnedspells"],
 	async execute(message, args, bot) {
-		const user = bot.getUserFromMention(args[0], message.guild) || message.guild.members.get(args[0]) || message.member;
+		const user = bot.functions.getUserFromMention(args[0], message.guild) || message.guild.members.cache.get(args[0]) || message.member;
 
-		const learnedSpells = await bot.userInfo.get(`${message.guild.id}-${user.id}`, "studiedSpells");
-		const spells = learnedSpells.sort().map(s => `**${s.charAt(0).toUpperCase() + s.slice(1)}** (${spellsFile.find(spell => spell.spellName === s).name})`).join("\n");
+		const learnedSpells = bot.userInfo.get(`${message.guild.id}-${user.id}`, "studiedSpells");
+		const spells = learnedSpells
+			.sort()
+			.map(s => `**${bot.functions.capitalizeFirstLetter(s)}** (${spellsFile.find(spell => spell.spellName == s).name})`)
+			.join("\n");
 
-		const spellsEmbed = new Discord.RichEmbed()
-			.setAuthor(`${user.displayName}'s Learned Spells/Potions`, user.user.displayAvatarURL)
+		const oneTimeSpells = ["herbology", "care of magical creatures"]; // These spells automatically reset when studied and can't be permanently learned.
+
+		const spellsEmbed = new Discord.MessageEmbed()
+			.setAuthor(`${user.displayName}'s Learned Spells/Potions`, user.user.displayAvatarURL())
 			.setColor(user.displayHexColor)
 			.setDescription(spells)
-			.setFooter(`${learnedSpells.length}/${spellsFile.filter(s => !["herbology", "care of magical creatures"].includes(s.class.toLowerCase())).length} spells learned`)
+			.setFooter(`${learnedSpells.length}/${spellsFile.filter(s => !oneTimeSpells.includes(s.class.toLowerCase())).length} spells learned`)
 			.setTimestamp();
 		message.channel.send(spellsEmbed);
 	},
