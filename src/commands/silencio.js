1d04a417c6c6f7c749ea4493ab7b7e3c212036a0
StrangeAlmond <strangealmond@strangealmond.com>
2020-04-28 20:59:59 -0700

    Updates the silencio and sonorus commands

--- src/commands/silencio.js
+++ src/commands/silencio.js
@@ -1,6 +1,6 @@
+// TODO: Test command
 const Discord = require("discord.js");
 const moment = require("moment-timezone");
-const ms = require("parse-ms");
 
 module.exports = {
 	name: "silencio",
@@ -9,16 +9,22 @@ module.exports = {
 	async execute(message, args, bot) {
 		if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
 
-		const userToMute = bot.getUserFromMention(args[0], message.guild) || message.guild.members.get(args[0]);
-		if (!userToMute) return message.channel.send(`Specify a user to mute! Proper Usage: \`${bot.prefix}silencio <@member> <hours:minutes:seconds> <reason>\``);
+		const userToMute = bot.functions.getUserFromMention(args[0], message.guild) || message.guild.members.cache.get(args[0]);
+		if (!userToMute) {
+			return message.channel.send(`Specify a user to mute! Proper Usage: \`${bot.prefix}silencio <@member> <hours:minutes:seconds> <reason>\``);
+		}
+
+		// if (userToMute.user.bot) return message.channel.send("You can't mute a bot!");
 
 		if (userToMute.hasPermission("MANAGE_MESSAGES")) return;
-		if (userToMute.roles.find(r => r.name.toLowerCase() === "silenced")) return message.channel.send(`${userToMute.displayName} has already been muted!`);
+		if (userToMute.roles.cache.find(r => r.name.toLowerCase() == "silenced")) {
+			return message.channel.send(`${userToMute.displayName} has already been muted!`);
+		}
 
-		bot.ensureUser(userToMute);
+		bot.functions.ensureUser(userToMute, bot);
 
 		let temporary = true;
-		let time = args[1].match(/\d+:\d+:\d+/g);
+		let time = args[1].match(/\d+:\d+:\d+/g); // Matches hours:minutes:seconds
 		if (!time) temporary = false;
 
 		const reason = temporary ? args.slice(2).join(" ") : args.slice(1).join(" ");
@@ -30,7 +36,11 @@ module.exports = {
 			const minutes = time[1];
 			const seconds = time[2];
 
-			const unmuteTime = moment.tz("America/Los_Angeles").add(hours, "hours").add(minutes, "minutes").add(seconds, "seconds");
+			const unmuteTime = moment
+				.tz(bot.timezone)
+				.add(hours, "hours")
+				.add(minutes, "minutes")
+				.add(seconds, "seconds");
 
 			const muteObject = {
 				unmuteTime: unmuteTime,
@@ -42,24 +52,26 @@ module.exports = {
 
 		bot.userInfo.inc(`${message.guild.id}-${userToMute.id}`, "stats.mutes");
 
-		const timeObject = ms(bot.userInfo.get(`${message.guild.id}-${userToMute.id}`, "muteObject.unmuteTime") - Date.now());
+		const timeObject = bot.functions.parseMs(bot.userInfo.get(`${message.guild.id}-${userToMute.id}`, "muteObject.unmuteTime") - Date.now(), true);
 
-		const silencedRole = message.guild.roles.find(r => r.name.toLowerCase() === "silenced");
-		userToMute.addRole(silencedRole);
+		const silencedRole = message.guild.roles.cache.find(r => r.name.toLowerCase() == "silenced");
+		userToMute.roles.add(silencedRole);
 
-		const logChannel = message.guild.channels.find(c => c.name === "incidents");
+		const logChannel = message.guild.channels.cache.find(c => c.name == "incidents");
 
-		const embed = new Discord.RichEmbed()
-			.setAuthor(`${userToMute.displayName} has been muted`, userToMute.user.displayAvatarURL)
-			.setDescription(`**Time:** ${temporary ? `${timeObject.hours} hours, ${timeObject.minutes} minutes, and ${timeObject.seconds} seconds` : "Permanent"}\n**Reason:** ${reason}`)
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(`${userToMute.displayName} has been muted`, userToMute.user.displayAvatarURL())
+			.setDescription(`**Time:** ${temporary ? `${timeObject.hours}h ${timeObject.minutes}m ${timeObject.seconds}s` : "Permanent"}\n**Reason:** ${reason}`)
 			.setColor("#DD889F")
 			.setTimestamp();
 
-		bot.quickWebhook(logChannel, embed, {
+		bot.functions.quickWebhook(logChannel, embed, {
 			username: "Dolores Umbridge",
-			avatar: "./images/webhook_avatars/doloresUmbridge.png"
+			avatar: "../images/webhook_avatars/doloresUmbridge.png"
 		});
 
-		message.channel.send(`${userToMute.displayName} has been ${!temporary ? "permanently muted" : `muted for **${timeObject.hours} hours**, **${timeObject.minutes} minutes**, and **${timeObject.seconds} seconds**`} for **${reason}**`);
+		message.channel.send(`${userToMute.displayName} has been ${!temporary ?
+			"permanently muted" :
+			`muted for **${timeObject.hours}h**, **${timeObject.minutes}m**, and **${timeObject.seconds}s**`} for **${reason}**`);
 	},
 };
--- src/commands/sonorus.js
+++ src/commands/sonorus.js
@@ -1,3 +1,4 @@
+// TODO: Test command
 const Discord = require("discord.js");
 
 module.exports = {
@@ -7,33 +8,36 @@ module.exports = {
 	async execute(message, args, bot) {
 		if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
 
-		const userToUnmute = bot.getUserFromMention(args[0], message.guild) || message.guild.members.get(args[0]);
-		if (!userToUnmute) return message.channel.send(`Specify a user to unmute! Proper Usage: \`${bot.prefix}sonorus <@member>\``);
+		const userToUnmute = bot.functions.getUserFromMention(args[0], message.guild) || message.guild.members.cache.get(args[0]);
+		if (!userToUnmute) {
+			return message.channel.send(`Specify a user to unmute! Proper Usage: \`${bot.prefix}sonorus <@member>\``);
+		}
 
-		if (userToUnmute.hasPermission("MANAGE_MESSAGES")) return;
-		if (!userToUnmute.roles.find(r => r.name.toLowerCase() === "silenced")) return message.channel.send("They haven't been silenced!");
+		if (!userToUnmute.roles.cache.find(r => r.name.toLowerCase() == "silenced")) {
+			return message.channel.send("They haven't been silenced!");
+		}
 
-		const incidentsChannel = message.guild.channels.find(c => c.name === "incidents");
+		const incidentsChannel = message.guild.channels.cache.find(c => c.name == "incidents");
 		if (!incidentsChannel) return;
 
 		const webhookOptions = {
 			username: "Dolores Umbridge",
-			avatar: "./images/webhook_avatars/doloresUmbridge.png"
+			avatar: "../images/webhook_avatars/doloresUmbridge.png"
 		};
 
-		const mutedRole = await message.guild.roles.find(r => r.name === "Silenced");
-		await userToUnmute.removeRole(mutedRole);
+		const mutedRole = await message.guild.roles.cache.find(r => r.name == "Silenced");
+		await userToUnmute.roles.remove(mutedRole);
 
 		bot.userInfo.set(`${message.guild.id}-${userToUnmute.id}`, null, "muteObject");
 
-		const embed = new Discord.RichEmbed()
-			.setTitle(`${userToUnmute.displayName} has been unmuted`)
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(`${userToUnmute.displayName} has been unmuted`, userToUnmute.user.displayAvatarURL())
 			.setColor("#633039")
 			.setDescription(`**User:** ${userToUnmute.displayName}\n**Moderator:** ${message.member.displayName}`)
 			.setFooter(`${message.member.displayName} unmuted ${userToUnmute.displayName}`)
 			.setTimestamp();
 
-		bot.quickWebhook(incidentsChannel, embed, webhookOptions);
+		bot.functions.quickWebhook(incidentsChannel, embed, webhookOptions);
 		message.channel.send(`${userToUnmute.displayName} has been unmuted`);
 	},
 };
