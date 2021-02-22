fec9df1f0b0161f5a8cb7c4d211c337a3d80fa86
StrangeAlmond <strangealmond@strangealmond.com>
2021-02-22 17:03:23 -0500

    Change welcome message when a user joins the guild

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -29,22 +29,22 @@ module.exports = async (bot, member) => {
 
   channel.send(`${member}, Welcome to Diagon Alley!
   
-Here, you'll find everythin' yeh need to get started. Firs' things firs', look at the menu on the left to see all the shops 'ere in Diagon Alley. Before yeh can start shoppin', you'll need to go to ${gringotts} bank an' !withdraw some funds so yeh can buy yer school supplies.
+Here, you'll find everythin' yeh need to get started. Firs' things firs', look at the menu on the left to see all the shops 'ere in Diagon Alley. Before yeh can start shoppin', you'll need to go to ${gringotts} bank an' **${bot.prefix}withdraw** some funds so yeh can buy yer school supplies.
   
 Affer that, make sure to visit all the shops to gather the required items for yer firs' year at Hogwarts. After yer done shoppin, yeh'll be able to board the Hogwarts Express. Here's a list of what yeh need and where to find it:
   
 Wand - ${ollivanders}
-${bot.prefix}buy wand
+**${bot.prefix}buy wand**
   
 School Books - ${flourishAndBlotts}
-${bot.prefix}buy books
+**${bot.prefix}buy books**
   
 Robes/Hat/Gloves - ${madamMalkins}
-${bot.prefix}buy clothes
+**${bot.prefix}buy clothes**
   
 Cauldron (Pewter, standard size 2) - ${potages}
-${bot.prefix}buy cauldron
+**${bot.prefix}buy cauldron**
   
 Crystal phials, telescope, brass scales - ${wisacres}
-${bot.prefix}buy supplies`);
+**${bot.prefix}buy supplies**`);
 };
