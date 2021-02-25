dbd952d53f318af4b8b1e62b974065a824f54d4e
StrangeAlmond <strangealmond@strangealmond.com>
2021-02-25 17:29:41 -0500

    Add footer and color to guild stats embed

--- src/commands/guild-stats.js
+++ src/commands/guild-stats.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const db = require("../utils/db.js");
+const moment = require("moment-timezone");
 
 module.exports = {
     name: "guild-stats",
@@ -9,7 +10,9 @@ module.exports = {
         const guildStats = db.guildInfo.get(message.guild.id, "stats");
         const statsEmbed = new Discord.MessageEmbed()
             .setAuthor(`${message.guild.name}'s Statistics`, message.guild.iconURL())
+            .setColor(message.member.displayHexColor)
             .setDescription(`**Total Training Sessions:** ${guildStats.trainingSessions}\n**Total Spawns:** ${guildStats.spawns}\n**Total Trivia Questions:** ${guildStats.triviaQuestions}`)
+            .setFooter(`Utilizing ${bot.user.username} since ${moment.tz(message.guild.me.joinedTimestamp, bot.timezone).format("llll")}`)
             .setTimestamp();
 
         message.channel.send(statsEmbed);
